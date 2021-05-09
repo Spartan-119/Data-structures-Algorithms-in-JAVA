e7322e5bc95ffe8804c65ec8b381f3981146b805
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2021-05-09 06:07:17 +0530

    Add files via upload

--- Stacks/StackDriver.java
+++ Stacks/StackDriver.java
@@ -2,9 +2,9 @@ public class StackDriver {
     public static void main (String [] args) {
         Stack s1 = new Stack();
 
-        s1.push(1);
-        s1.push(2);
-        s1.push(3);
+        for(int i = 1; i <= 10; i++){
+            s1.push(i);
+        }
         s1.showStack();
         System.out.println();
         System.out.println("Element at the top of the stack is: " + s1.pop());
